7f8f95cd1ba698e06dbf5f1078941853c59be4b7
hongpeichua <hongggpei@gmail.com>
2024-07-16 08:34:27 +0800

    Create FruitCard component

---
+++ frontend/src/components/FruitCard.jsx
@@ -0,0 +1,68 @@
+import React, { useContext } from "react";
+import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
+
+import { Card, Button, message } from "antd";
+
+import { ShoppingCartOutlined } from "@ant-design/icons";
+
+const { Meta } = Card;
+
+const FruitCard = (props) => {
+  const fetchData = useFetch();
+  const userCtx = useContext(UserContext);
+
+  const handleAddToCart = async () => {
+    try {
+      console.log("User Token:", userCtx.accessToken);
+      const res = await fetchData(
+        "/api/cart",
+        "PUT",
+        { user_id: userCtx.userId, fruit_id: props.id, quantity: 1 },
+        undefined,
+        userCtx.accessToken
+      );
+      if (res.ok) {
+        message.success(`Item added to cart!`);
+      } else {
+        console.error("Error adding item to cart:", res.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Card
+      style={{
+        width: 300,
+        margin: "30px",
+      }}
+      cover={<img src={props.image_url} style={{ padding: "30px" }} />}
+      actions={[
+        <Button
+          type="primary"
+          shape="round"
+          icon={<ShoppingCartOutlined />}
+          onClick={handleAddToCart}
+        >
+          Add to Cart
+        </Button>,
+        ,
+      ]}
+    >
+      <Meta
+        title={`${props.name} ($${props.price} /qty)`}
+        description={props.description}
+      />
+      <br></br>
+      <Meta
+        description={
+          <div style={{ textAlign: "right" }}>{props.stocks} stocks</div>
+        }
+      />
+    </Card>
+  );
+};
+
+export default FruitCard;
