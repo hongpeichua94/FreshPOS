50810b711c89bc40cf7a1f54d774a6b9bc7fcbd9
hongpeichua <hongggpei@gmail.com>
2024-08-02 14:09:38 +0800

    Update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,11 +1,25 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: "localhost",
+//   port: 5432,
+//   database: "freshfruits",
+// });
+
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: "localhost",
+  host: "dpg-cqm7973qf0us73a6h7r0-a",
   port: 5432,
-  database: "freshfruits",
+  database: "freshfruits_database",
 });
 
 module.exports = {
