daae9b3c2cb796ced690873705d16c638a601e7b
hongpeichua <hongggpei@gmail.com>
2024-10-24 16:58:21 +0800

    Update useFetch.jsx
    Update to handle formData

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -2,13 +2,25 @@ const useFetch = () => {
   const fetchData = async (endpoint, method, body) => {
     const token = localStorage.getItem("accessToken");
 
+    // Set up headers conditionally based on the body type
+    const headers = {
+      Authorization: "Bearer " + token,
+    };
+
+    // If the body is not formData, assume it's json and add the appropriate headers
+    if (body && !(body instanceof FormData)) {
+      headers["Content-Type"] = "application/json";
+    }
+
     const res = await fetch(import.meta.env.VITE_SERVER + endpoint, {
       method,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
-      },
-      body: JSON.stringify(body),
+      headers,
+      body: body && !(body instanceof FormData) ? JSON.stringify(body) : body,
+      // headers: {
+      //   "Content-Type": "application/json",
+      //   Authorization: "Bearer " + token,
+      // },
+      // body: JSON.stringify(body),
     });
     const data = await res.json();
 
