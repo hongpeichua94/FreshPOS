c2321a2da2d585c6794a2f3bcb9fb10b6c4f1cfb
hongpeichua <hongggpei@gmail.com>
2024-10-25 01:21:29 +0800

    Update UpdateInventoryModal.jsx

--- frontend/src/components/UpdateInventoryModal.jsx
+++ frontend/src/components/UpdateInventoryModal.jsx
@@ -1,10 +1,20 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useState } from "react";
 import ReactDOM from "react-dom";
 import useFetch from "../hooks/useFetch";
 import UserContext from "../context/user";
 
 // ANT DESIGN
-import { Button, message } from "antd";
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+  Space,
+  Upload,
+  message,
+} from "antd";
+import { UploadOutlined, CloseOutlined } from "@ant-design/icons";
 
 // MODULE CSS
 import styles from "./Modal.module.css";
@@ -13,13 +23,33 @@ const OverLay = (props) => {
   const fetchData = useFetch();
   const userCtx = useContext(UserContext);
 
-  const imageRef = useRef("");
-  const nameRef = useRef("");
-  const descriptionRef = useRef("");
-  const priceRef = useRef("");
-  const quantityRef = useRef("");
+  const [error, setError] = useState("");
+  const [form] = Form.useForm();
 
-  const updateInventory = async (id) => {
+  const { Option } = Select;
+  const formItemLayout = {
+    labelCol: {
+      span: 6,
+    },
+    wrapperCol: {
+      span: 14,
+    },
+  };
+
+  const normFile = (e) => {
+    console.log("Upload event:", e);
+    if (Array.isArray(e)) {
+      return e.slice(-1); // Keep only the last file (single file upload)
+    }
+    return e?.fileList.slice(-1); // Ensure that only the last file is kept
+  };
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+    updateInventory(props.id, values);
+  };
+
+  const updateInventory = async (id, values) => {
     const confirmUpdate = confirm(
       "Are you sure you want to proceed to update profile details?"
     );
@@ -28,45 +58,181 @@ const OverLay = (props) => {
       return; // Do nothing if user cancels
     }
 
-    const res = await fetchData(
-      `/api/fruit/${id}`,
-      "PATCH",
-      {
-        name: nameRef.current.value,
-        description: descriptionRef.current.value,
-        image_url: descriptionRef.current.value,
-        price: priceRef.current.value,
-        quantity: quantityRef.current.value,
-      },
-      userCtx.accessToken
-    );
+    const { name, description, image, price, quantity } = values;
 
-    if (res.ok) {
-      message.success("Inventory updated successfully!");
-      props.fetchInventoryData();
-      props.setShowInventoryUpdateModal(false);
-    } else {
-      alert(JSON.stringify(res.data));
-      console.log(res.data);
+    try {
+      // Create FormData object
+      const formData = new FormData();
+
+      // Append data if provided
+      if (image) {
+        formData.append("file", image[0].originFileObj);
+      }
+      if (name) {
+        formData.append("name", name);
+      }
+      if (description) {
+        formData.append("description", description);
+      }
+      if (price) {
+        formData.append("price", price);
+      }
+      if (quantity) {
+        formData.append("quantity", quantity);
+      }
+
+      const res = await fetchData(
+        `/api/fruit/${id}`,
+        "PATCH",
+        formData,
+        // {
+        //   name: nameRef.current.value,
+        //   description: descriptionRef.current.value,
+        //   image_url: imageRef.current.value,
+        //   price: priceRef.current.value,
+        //   quantity: quantityRef.current.value,
+        // },
+        userCtx.accessToken
+      );
+
+      if (res.ok) {
+        message.success("Inventory updated successfully!");
+        props.fetchInventoryData();
+        props.setShowInventoryUpdateModal(false);
+      } else {
+        alert(JSON.stringify(res.data));
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.error(error);
+      setError("An error occurred during inventory update");
     }
   };
-
-  useEffect(() => {
-    imageRef.current.value = props.imageUrl;
-    nameRef.current.value = props.name;
-    descriptionRef.current.value = props.description;
-    priceRef.current.value = props.price;
-    quantityRef.current.value = props.quantity;
-  }, []);
 
   return (
     <div className={styles.backdrop}>
       <div className={styles.modal}>
         <div className={styles.header}>
           <h4 className="modal-title">Update inventory</h4>
+          <Button
+            className={styles.closeButton}
+            onClick={() => props.setShowInventoryUpdateModal(false)}
+            icon={<CloseOutlined />}
+            type="text"
+          />
         </div>
 
-        <div className={styles.body}>
+        <Form
+          form={form}
+          encType="multipart/form-data"
+          name="validate_other"
+          {...formItemLayout}
+          onFinish={onFinish}
+          initialValues={{
+            name: props.name,
+            description: props.description,
+            price: props.price,
+            quantity: props.quantity,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+        >
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[
+              {
+                required: true,
+                message: "Please enter fruit name!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="description"
+            label="Description"
+            rules={[
+              {
+                required: true,
+                message: "Please enter a description!",
+              },
+            ]}
+          >
+            <Input.TextArea rows={4} />
+          </Form.Item>
+
+          <Form.Item
+            name="image"
+            label="Image"
+            valuePropName="fileList"
+            getValueFromEvent={normFile}
+            // rules={[{ required: true, message: "Please upload an image!" }]}
+          >
+            <>
+              <Upload
+                name="file"
+                accept="image/*"
+                beforeUpload={() => false}
+                maxCount={1}
+              >
+                <Button icon={<UploadOutlined />}>Upload Image</Button>
+                {props.image && (
+                  <div style={{ marginTop: "5px" }}>
+                    <span>
+                      {/* Display the file name */}
+                      {props.image.split("/").pop()}{" "}
+                    </span>
+                  </div>
+                )}
+              </Upload>
+            </>
+          </Form.Item>
+
+          <Form.Item
+            name="price"
+            label="Price"
+            rules={[
+              {
+                required: true,
+                message: "Please enter a price!",
+              },
+            ]}
+          >
+            <InputNumber />
+          </Form.Item>
+
+          <Form.Item
+            name="quantity"
+            label="Quantity"
+            rules={[
+              {
+                required: true,
+                message: "Please enter a quantity!",
+              },
+            ]}
+          >
+            <InputNumber />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              span: 12,
+              offset: 6,
+            }}
+          >
+            <Space>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+              <Button htmlType="reset">reset</Button>
+            </Space>
+          </Form.Item>
+        </Form>
+
+        {/* <div className={styles.body}>
           <div className="row">
             <div className="col-md-1"></div>
             <div className="col-md-3">Fruit</div>
@@ -101,18 +267,7 @@ const OverLay = (props) => {
             <input ref={quantityRef} type="text" className="col-md-6" />
             <div className="col-md-3"></div>
           </div>
-        </div>
-        <br />
-        <div className={`row ${styles.footer}`}>
-          <div className="col-md-12 d-flex justify-content-center">
-            <Button onClick={() => props.setShowInventoryUpdateModal(false)}>
-              Close
-            </Button>
-            <Button type="primary" onClick={() => updateInventory(props.id)}>
-              Save changes
-            </Button>
-          </div>
-        </div>
+        </div> */}
         <br />
       </div>
     </div>
@@ -127,7 +282,7 @@ const UpdateInventoryModal = (props) => {
           id={props.id}
           name={props.name}
           description={props.description}
-          imageUrl={props.imageUrl}
+          image={props.image}
           price={props.price}
           quantity={props.quantity}
           sold={props.sold}
